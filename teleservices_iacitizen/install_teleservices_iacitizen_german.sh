8841ad2b322f18142b7014142adfea5e9c257bd8
Daniel Muyshond <daniel.muyshond@imio.be>
2023-11-24 09:44:24 +0100

    [MCITISVIA-22] Adapt sh install

--- teleservices_iacitizen/install_teleservices_iacitizen_german.sh
+++ teleservices_iacitizen/install_teleservices_iacitizen_german.sh
@@ -3,6 +3,6 @@
 set -e # Exit immediately if a command exits with a non-zero status.
 
 # installation path
-install_path="/usr/lib/teleservices_iacitizen"
+install_path="/usr/lib/teleservices_iacitizen_german"
 
 sudo -u hobo hobo-manage imio_indus_deploy --directory $install_path
